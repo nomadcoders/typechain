f53e734f2471d84f2fc182b5329577e87f8adc70
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-22 15:50:36 +0300

    #12 Conclusions

--- src/index.ts
+++ src/index.ts
@@ -96,4 +96,10 @@ const addBlock = (candidateBlock: Block): void => {
   }
 };
 
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
+
 export {};
