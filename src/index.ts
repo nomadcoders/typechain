215970710bc10fdd06c16580fcd5231d6633d76a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-22 15:01:05 +0300

    #6 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "nicolas",
-  age: 22,
-  gender: "male"
-};
+const lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${
@@ -16,6 +17,6 @@ const sayHi = (person: Human): string => {
   }!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
